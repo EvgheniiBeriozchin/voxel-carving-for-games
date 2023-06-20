f91b13000793ca4af7b4dfd72a8dd0f11feec6ba
Achim Bunke <achim.bunke@googlemail.com>
2023-06-20 22:58:45 +0200

    added opencv include

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#define RunOpenCVTest 1
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+using namespace cv;
+
 int main() {
+	if (RunOpenCVTest) {
+		namedWindow("Test_window", WINDOW_AUTOSIZE);
+		while (waitKey(100) <= 0) {
+
+		}
+	}
 	return 0;
 }
