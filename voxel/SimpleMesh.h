0a8471dcd19f622e5f089e9550b291412a973e5f
Lars Eble <eble.lars@gmail.com>
2023-07-19 20:14:52 +0200

    Add libigl Add tutte embedding algorithm

--- voxel/SimpleMesh.h
+++ voxel/SimpleMesh.h
@@ -22,6 +22,13 @@ struct Triangle
 	{}
 };
 
+
+
+struct IglInputFormat {
+	Eigen::MatrixXd V;
+	Eigen::MatrixXi F;
+};
+
 class SimpleMesh
 {
 public:
@@ -56,6 +63,23 @@ public:
 	{
 		return m_triangles;
 	}
+
+	IglInputFormat GetIglInputFormat()
+	{
+		IglInputFormat iglInputFormat;
+		iglInputFormat.V.resize(m_vertices.size(), 3);
+		iglInputFormat.F.resize(m_triangles.size(), 3);
+		for (unsigned int i = 0; i < m_vertices.size(); i++)
+		{
+			iglInputFormat.V.row(i) = m_vertices[i];
+		}
+		for (unsigned int i = 0; i < m_triangles.size(); i++)
+		{
+			iglInputFormat.F.row(i) = Eigen::Vector3i(m_triangles[i].idx0, m_triangles[i].idx1, m_triangles[i].idx2);
+		}
+		return iglInputFormat;
+	}
+
 
 	bool WriteMesh(const std::string& filename)
 	{
